1cdb8f835ae029ed1dd5504a8324865128420bd1
Juan <juancblasich@gmail.com>
2020-09-12 15:07:41 -0300

    Cambio de contraseña funcionando. Controles agregados.

--- src/main/java/com/remiNorte/app/controllers/PasswordController.java
+++ src/main/java/com/remiNorte/app/controllers/PasswordController.java
@@ -12,6 +12,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,48 +47,53 @@ public class PasswordController {
     
     // Process form submission from forgotPassword page
 	@RequestMapping(value = "/forgotPassword", method = RequestMethod.POST)
-	public String processForgotPasswordForm(Model model,
-											@RequestParam("email") String userEmail, 
+	public String processForgotPasswordForm(Model model,											
+											@RequestParam("email") String userEmail,
 											HttpServletRequest request, 
 											RedirectAttributes flash) {
-
-		// Lookup user in database by e-mail
-		Usuario optional = null;
-		optional = userService.findByUsername(userEmail);
 		
-		if (optional == null) {
-			//modelAndView.addObject("errorMessage", "We didn't find an account for that e-mail address.");
+		if (userEmail == null || userEmail == "") {
+			flash.addFlashAttribute("errorMail","Debe ingresar un E-Mail.");
+			return "redirect:forgotPassword";
 		} else {
 			
-			// Generate random 36-character string token for reset password 
-			Usuario user = optional;
-			user.setUsuResTok(UUID.randomUUID().toString());
-
-			// Save token to database
-			userService.save(user);
-
-			String appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getLocalPort();
-			logger.info(appUrl);
+			// Lookup user in database by e-mail
+			Usuario optional = null;
+			optional = userService.findByUsername(userEmail);
 			
-			// Email message
-			SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
-			passwordResetEmail.setFrom("support@demo.com");
-			passwordResetEmail.setTo(user.getUsername());
-			passwordResetEmail.setSubject("Password Reset Request");
-			passwordResetEmail.setText("To reset your password, click the link below:\n" + appUrl
-					+ "/resetPassword?token=" + user.getUsuResTok());
+			if (optional == null) {
+				//modelAndView.addObject("errorMessage", "We didn't find an account for that e-mail address.");
+			} else {
+				
+				// Generate random 36-character string token for reset password 
+				Usuario user = optional;
+				user.setUsuResTok(UUID.randomUUID().toString());
+	
+				// Save token to database
+				userService.save(user);
+	
+				String appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getLocalPort();
+				logger.info(appUrl);
+				
+				// Email message
+				SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
+				passwordResetEmail.setFrom("remiserianorte@gmail.com");
+				passwordResetEmail.setTo(user.getUsername());
+				passwordResetEmail.setSubject("Remisería Norte: cambio de contraseña.");
+				passwordResetEmail.setText("Para reiniciar tu contraseña, click en la dirección de abajo:\n" + appUrl
+						+ "/resetPassword?token=" + user.getUsuResTok());
+				
+				emailService.sendEmail(passwordResetEmail);
+	
+				// Add success message to view
+				//modelAndView.addObject("successMessage", "A password reset link has been sent to " + userEmail);
+			}
 			
-			emailService.sendEmail(passwordResetEmail);
-
-			// Add success message to view
-			//modelAndView.addObject("successMessage", "A password reset link has been sent to " + userEmail);
+			flash.addFlashAttribute("success","Si el E-Mail es válido, recibirá un correo de reinicio de contraseña.");
+			//modelAndView.setViewName("inicio");
+			//return modelAndView;
+			return "redirect:inicio";
 		}
-		
-		flash.addFlashAttribute("success","Si el E-Mail es válido, recibirá un correo de reinicio de contraseña.");
-		//modelAndView.setViewName("inicio");
-		//return modelAndView;
-		return "redirect:inicio";
-
 	}
 
 	// Display form to reset password
@@ -96,20 +102,14 @@ public class PasswordController {
 												@RequestParam("token") String token) {
 		
 		Usuario user = null;
-		logger.info("token GET:".concat(token));
 		user = userService.findByUsuResTok(token);
 
 		if (user != null) { // Token found in DB
-			logger.info("user encontrado");
-			//modelAndView.addObject("token", token);
 			model.addAttribute("token", token);
 		} else { // Token not found in DB
-			//modelAndView.addObject("errorMessage", "Oops!  This is an invalid password reset link.");
-			model.addAttribute("error", "Oops!  This is an invalid password reset link.");
+			model.addAttribute("error", "Oops!  Este es un link de reinicio de contraseña inválido.");
 		}
-
-		//modelAndView.setViewName("resetPassword");
-		//return modelAndView;
+		
 		return "resetPassword";
 	}
 
@@ -122,43 +122,49 @@ public class PasswordController {
 								@RequestParam("confirmPassword") String confirmPassword,
 								@RequestParam("token") String token) {
 		
-		// Find the user associated with the reset token
-		Usuario user = null;
-		logger.info("token POST:".concat(token));
-		
-		user = userService.findByUsuResTok(token);
-
-		// This should always be non-null but we check just in case
-		if (user != null) {
-			
-			Usuario resetUser = user;
-            
-			// Set new password    
-            resetUser.setPassword(bCryptPasswordEncoder.encode(password));
-            
-			// Set the reset token to null so it cannot be used again
-			resetUser.setUsuResTok(null);
-
-			// Save user
-			userService.save(resetUser);
-
-			// In order to set a model attribute on a redirect, we must use
-			// RedirectAttributes
-			redir.addFlashAttribute("success", "La contraseña fue cambiada exitosamente.");
-
-			//modelAndView.setViewName("redirect:login");
-			//return modelAndView;
-			return "redirect:login";
-			
+		if (password == null || password == "") {
+			redir.addFlashAttribute("ePassword", "Debe ingresar una contraseña.");
+			return "redirect:resetPassword?token="+token.trim();
 		} else {
-			//modelAndView.addObject("error", "Oops!  This is an invalid password reset link.");
-			//modelAndView.setViewName("resetPassword");
-			
-			redir.addFlashAttribute("error", "Oops!  Este es un link de reinicio de contraseña inválido.");
-			return "redirect:inicio";
-		}
-		
-		//return "redirecct:login";
+			if (!password.equals(confirmPassword)) {
+				redir.addFlashAttribute("eConfirmPassword", "Las contraseñas no coinciden.");
+				return "redirect:resetPassword?token="+token.trim();
+			} else {
+				// Find the user associated with the reset token
+				Usuario user = null;		
+				user = userService.findByUsuResTok(token);
+
+				// This should always be non-null but we check just in case
+				if (user != null) {
+					
+					Usuario resetUser = user;
+		            
+					// Set new password    
+		            resetUser.setPassword(bCryptPasswordEncoder.encode(password));
+		            
+					// Set the reset token to null so it cannot be used again
+					resetUser.setUsuResTok(null);
+
+					// Save user
+					userService.save(resetUser);
+
+					// In order to set a model attribute on a redirect, we must use
+					// RedirectAttributes
+					redir.addFlashAttribute("success", "La contraseña fue cambiada exitosamente.");
+
+					//modelAndView.setViewName("redirect:login");
+					//return modelAndView;
+					return "redirect:login";
+					
+				} else {
+					//modelAndView.addObject("error", "Oops!  This is an invalid password reset link.");
+					//modelAndView.setViewName("resetPassword");
+					
+					redir.addFlashAttribute("error", "Oops!  Este es un link de reinicio de contraseña inválido.");
+					return "redirect:inicio";
+				}
+			}
+		}		
    }
    
     // Going to reset page without a token redirects to login page
